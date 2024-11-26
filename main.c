beac8ffa9d546415335be7188ac5a275ab122630
Oskar K <oskarkloczko13@gmail.com>
2024-11-26 09:21:26 +0100

    added input/output client functionality

--- main.c
+++ main.c
@@ -2,37 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <winsock2.h>
-#include <string.h>
 #include <windows.h>
+#include <conio.h>
+
 
 #pragma comment(lib, "ws2_32.lib") // Link with ws2_32.lib
 
 #define BUFFER_LEN 512
 
 #define SERVER "irc.undernet.org"  
-#define PORT 6667                  
+#define PORT 6667                   
 #define CHANNEL "#testchannel"     
 #define NICKNAME "TestBot"         
 #define USERNAME "TestBot"   
 #define CLIENT_EMAIL "irc295352@gmail.com" 
 #define CLIENT_PASSWORD "testingircclient"    
 
-// HANDLE mutex;
 SOCKET sockfd;
-
-DWORD WINAPI ThreadFunction(LPVOID lpParam){
-    char send_buffer[BUFFER_LEN];
-    while(1){
-        printf("Enter message: ");
-        fgets(send_buffer, BUFFER_LEN, stdin);
-        send_buffer[strcspn(send_buffer, "\n")] = '\0';
-        // WaitForSingleObject(mutex, INFINITE);
-        send(sockfd, send_buffer, strlen(send_buffer), 0);
-        // ReleaseMutex(mutex); 
-    }
-
-    return 0;
-}
 
 void send_message(SOCKET sockfd, const char *message) {
     send(sockfd, message, strlen(message), 0);
@@ -45,35 +31,37 @@ void receive_messages(SOCKET sockfd) {
     int bytesReceived;
     char *check = "PING ";
 
-    while (1) {
-        // WaitForSingleObject(mutex, INFINITE);
-        bytesReceived = recv(sockfd, buffer, sizeof(buffer) - 1, 0);
-        if (bytesReceived < 1) {
-            printf("Connection closed or error\n");
-            break;
+    bytesReceived = recv(sockfd, buffer, sizeof(buffer) - 1, 0);
+    if (bytesReceived < 0) {
+        int error = WSAGetLastError();
+        if (error == WSAEWOULDBLOCK) {
+            // No data to read, just return
+            return;
+        } else {
+            printf("recv failed with error: %d\n", error);
+            exit(1);
         }
-        buffer[bytesReceived] = '\0';
-        printf("Received: %s", buffer);
-        // ANSWERING TO PING RESPONSE
-        if (strstr(buffer, check)!=NULL){
-            char *ping_message = strstr(buffer, check);
-            if (ping_message != NULL) {
-                char *ping_id = strchr(ping_message, ':');
-                if (*ping_id != '\0') {
-                    snprintf(potential_message, sizeof(potential_message), "PONG %s\r\n", ping_id);
-                    send_message(sockfd, potential_message);
-                } else {
-                    printf("Error fetching the PING ID.\n");
-                }
+    } else if (bytesReceived == 0) {
+        printf("Connection closed by peer\n");
+        exit(1);
+    }
+    buffer[bytesReceived] = '\0';
+    printf("Received: %s", buffer);
+
+    // Respond to PING command
+    if (strstr(buffer, check) != NULL) {
+        char *ping_message = strstr(buffer, check);
+        if (ping_message != NULL) {
+            char *ping_id = strchr(ping_message, ':');
+            if (*ping_id != '\0') {
+                snprintf(potential_message, sizeof(potential_message), "PONG %s\r\n", ping_id);
+                send_message(sockfd, potential_message);
             } else {
-                printf("Error fetching ':' in the PING command.\n");
+                printf("Error fetching the PING ID.\n");
             }
         }
-        // ReleaseMutex(mutex);
     }
 }
-
-// void setup_irc(SOCKET socket,)
 
 int main() {
     WSADATA wsaData;
@@ -103,8 +91,6 @@ int main() {
         return 1;
     }
 
-    local_addr.sin_family = AF_INET;
-
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(PORT);
     serverAddr.sin_addr = *((struct in_addr *)host->h_addr_list[0]);
@@ -116,6 +102,10 @@ int main() {
         return 1;
     }
 
+    // u_long mode;
+    // ioctlsocket(sockfd, FIONBIO, &mode);  // Uncomment if you need to set the socket to unblocking mode
+    // printf("Socket mode: %lu\n", mode);
+
     if (getsockname(sockfd, (struct sockaddr *)&local_addr, &local_addr_len) == SOCKET_ERROR) {
         printf("getsockname failed. Error Code: %d\n", WSAGetLastError());
         closesocket(sockfd);
@@ -123,26 +113,7 @@ int main() {
         return 1;
     }
 
-    // mutex = CreateMutex(NULL, FALSE, NULL);
-    // if (mutex == NULL) {
-    //     printf("Mutex creation failed\n");
-    //     closesocket(sockfd);
-    //     WSACleanup();
-    //     return 1;
-    // }
-
-    HANDLE sending_thread = CreateThread(NULL, 0, ThreadFunction, NULL, 0, NULL);
-    HANDLE receiving_thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)receive_messages, (LPVOID)sockfd, 0, NULL);
-    if (sending_thread == NULL || receiving_thread == NULL) {
-        printf("Error creating threads\n");
-        closesocket(sockfd);
-        // CloseHandle(mutex);
-        WSACleanup();
-        return 1;
-    }
-
     local_port = ntohs(local_addr.sin_port);
-
     printf("Connected to %s on port %d\n", SERVER, PORT);
 
     snprintf(message, sizeof(message), "NICK %s\r\n", NICKNAME);
@@ -157,17 +128,48 @@ int main() {
     snprintf(message, sizeof(message), "113 :%s :%d\r\n", NICKNAME, local_port);
     send_message(sockfd, message);
 
+    fd_set read_fds, write_fds;
+    struct timeval timeout;
+    char input_buffer[BUFFER_LEN];
 
-    // snprintf(message, sizeof(message), "/msg NickServ REGISTER %s %s\r\n", CLIENT_EMAIL, CLIENT_PASSWORD);
-    // send_message(sockfd, message);
 
-    //receive_messages(sockfd);
-    WaitForSingleObject(sending_thread, INFINITE);
-    WaitForSingleObject(receiving_thread, INFINITE);
+    // Main Receive/Send Functionality
+    while (1) {
+        FD_ZERO(&read_fds);
+        FD_ZERO(&write_fds);
 
-    // FOR CLEANUP! DO NOT MODIFY
+        FD_SET(sockfd, &read_fds);
+        FD_SET(sockfd, &write_fds); 
+
+        timeout.tv_sec = 5;
+        timeout.tv_usec = 0;
+
+        int activity = select(sockfd + 1, &read_fds, &write_fds, NULL, &timeout);
+        if (activity == SOCKET_ERROR) {
+            printf("select() failed with error code: %d\n", WSAGetLastError());
+            break;
+        }
+
+        if (FD_ISSET(sockfd, &read_fds)) {
+            receive_messages(sockfd);
+        }
+
+        if (FD_ISSET(sockfd, &write_fds)) {
+            if (kbhit()) // Works perfectly for waiting for user input!
+            { 
+                fgets(input_buffer, BUFFER_LEN, stdin);
+                input_buffer[strcspn(input_buffer, "\n")] = '\0'; 
+
+                if (strlen(input_buffer) > 0) {
+                    char send_msg[BUFFER_LEN + 2];
+                    snprintf(send_msg, sizeof(send_msg), "%s\r\n", input_buffer);
+                    send_message(sockfd, send_msg);
+                }
+            }
+        }
+    }
+
     closesocket(sockfd);
-    // CloseHandle(mutex);
     WSACleanup();
 
     return 0;

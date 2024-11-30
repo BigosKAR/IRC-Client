8f4375d6557fb10c51557042793a5b3c0cde369e
Qamarr1 <qhussein.ieu2023@student.ie.edu>
2024-11-30 16:13:22 +0100

    Added main.c to feature-update branch

--- main.c
+++ main.c
@@ -1,159 +1,142 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <winsock2.h>
-#include <windows.h>
-#include <conio.h>
-
-
-#pragma comment(lib, "ws2_32.lib") // Link with ws2_32.lib
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
 #define BUFFER_LEN 512
+#define SERVER "irc.libera.chat"
+#define PORT 6667
+#define CHANNEL "#testchannel"
+#define NICKNAME "TestBot"
+#define USERNAME "TestBot"
 
-#define SERVER "irc.undernet.org"  
-#define PORT 6667                   
-#define CHANNEL "#testchannel"     
-#define NICKNAME "TestBot"         
-#define USERNAME "TestBot"   
-#define CLIENT_EMAIL "irc295352@gmail.com" 
-#define CLIENT_PASSWORD "testingircclient"    
+int sockfd;
 
-SOCKET sockfd;
+void format_command(const char *input, char *output) {
+    if (strncmp(input, "/privmsg", 8) == 0) {
+        const char *params = input + 8;
+        while (*params == ' ') params++;
 
-// SETUP FOR ALREADY REGISTERED USERS!
-void setup(SOCKET sockfd, struct sockaddr_in *local_address){
+        char target[BUFFER_LEN];
+        const char *message = strchr(params, ' ');
+
+        if (message) {
+            size_t target_len = message - params;
+            strncpy(target, params, target_len);
+            target[target_len] = '\0';
+            message++;
+            snprintf(output, BUFFER_LEN, "PRIVMSG %s :%s\r\n", target, message);
+        } else {
+            snprintf(output, BUFFER_LEN, "ERROR :Invalid PRIVMSG format\r\n");
+        }
+    } else if (strncmp(input, "/join", 5) == 0) {
+        const char *channel = input + 5;
+        while (*channel == ' ') channel++; // Skip leading spaces
+        snprintf(output, BUFFER_LEN, "JOIN %s\r\n", channel);
+    } else if (strncmp(input, "/nick", 5) == 0) {
+        const char *new_nick = input + 5;
+        while (*new_nick == ' ') new_nick++; // Skip leading spaces
+        snprintf(output, BUFFER_LEN, "NICK %s\r\n", new_nick);
+    } else {
+        snprintf(output, BUFFER_LEN, "%s\r\n", input);
+    }
+}
+
+
+
+void setup(int sockfd) {
     char message[BUFFER_LEN];
-    int local_port = ntohs(local_address->sin_port);
     printf("Connected to %s on port %d\n", SERVER, PORT);
 
     snprintf(message, sizeof(message), "NICK %s\r\n", NICKNAME);
     send(sockfd, message, strlen(message), 0);
+    sleep(1);
 
-    snprintf(message, sizeof(message), "USER %s 0 * :%s\r\n", USERNAME, USERNAME);
+    snprintf(message, sizeof(message), "USER %s 0 * :%s\r\n", USERNAME, "Test Bot Real Name");
     send(sockfd, message, strlen(message), 0);
+    sleep(1);
 
     snprintf(message, sizeof(message), "JOIN %s\r\n", CHANNEL);
     send(sockfd, message, strlen(message), 0);
-
-    snprintf(message, sizeof(message), "113 :%s :%d\r\n", NICKNAME, local_port);
-    send(sockfd, message, strlen(message), 0);
 }
 
-void send_message(SOCKET sockfd, const char *message) {
-    send(sockfd, message, strlen(message), 0);
+void send_message(int sockfd, const char *message) {
+    if (send(sockfd, message, strlen(message), 0) < 0) {
+        perror("Error sending message");
+    }
     printf("Sent: %s", message);
 }
 
-void receive_messages(SOCKET sockfd) {
-    char potential_message[512];
-    char buffer[512];
-    int bytesReceived;
-    char *check = "PING ";
-
-    bytesReceived = recv(sockfd, buffer, sizeof(buffer) - 1, 0);
-    if (bytesReceived < 0) {
-        int error = WSAGetLastError();
-        if (error == WSAEWOULDBLOCK) {
-            // No data to read, just return
-            return;
-        } else {
-            printf("recv failed with error: %d\n", error);
-            exit(1);
-        }
-    } else if (bytesReceived == 0) {
-        printf("Connection closed by peer\n");
+void receive_messages(int sockfd) {
+    char buffer[BUFFER_LEN];
+    int bytesReceived = recv(sockfd, buffer, sizeof(buffer) - 1, 0);
+    if (bytesReceived <= 0) {
+        printf("Connection closed or error occurred.\n");
         exit(1);
     }
     buffer[bytesReceived] = '\0';
     printf("Received: %s", buffer);
 
-    // Respond to PING command
-    if (strstr(buffer, check) != NULL) {
-        char *ping_message = strstr(buffer, check);
-        if (ping_message != NULL) {
-            char *ping_id = strchr(ping_message, ':');
-            if (*ping_id != '\0') {
-                snprintf(potential_message, sizeof(potential_message), "PONG %s\r\n", ping_id);
-                //send_message(sockfd, potential_message); HIDE PONG MESSAGE FROM USER
-                send(sockfd, potential_message, strlen(potential_message), 0);
-            } else {
-                printf("Error fetching the PING ID.\n");
-            }
+    if (strncmp(buffer, "PING", 4) == 0) {
+        char *ping_id = strchr(buffer, ':');
+        if (ping_id) {
+            char pong_response[BUFFER_LEN];
+            snprintf(pong_response, sizeof(pong_response), "PONG %s\r\n", ping_id + 1);
+            send_message(sockfd, pong_response);
         }
     }
 }
 
 int main() {
-    WSADATA wsaData;
-    SOCKET sockfd;
-    struct sockaddr_in serverAddr, local_addr;
+    struct sockaddr_in serverAddr;
     struct hostent *host;
-    char message[512];
-    int local_addr_len = sizeof(local_addr);
-    int local_port;
+    fd_set read_fds;
+    char input_buffer[BUFFER_LEN];
+    struct timeval timeout;
 
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        printf("WSAStartup failed. Error Code: %d\n", WSAGetLastError());
-        return 1;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("Socket creation failed");
+        exit(1);
     }
 
     host = gethostbyname(SERVER);
-    if (host == NULL) {
-        printf("Error resolving hostname: %s\n", SERVER);
-        WSACleanup();
-        return 1;
-    }
-
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == INVALID_SOCKET) {
-        printf("Socket creation failed. Error Code: %d\n", WSAGetLastError());
-        WSACleanup();
-        return 1;
+    if (!host) {
+        perror("Error resolving hostname");
+        close(sockfd);
+        exit(1);
     }
 
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(PORT);
-    serverAddr.sin_addr = *((struct in_addr *)host->h_addr_list[0]);
+    memcpy(&serverAddr.sin_addr, host->h_addr_list[0], host->h_length);
 
-    if (connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
-        printf("Connection failed. Error Code: %d\n", WSAGetLastError());
-        closesocket(sockfd);
-        WSACleanup();
-        return 1;
+    if (connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
+        perror("Connection failed");
+        close(sockfd);
+        exit(1);
     }
 
-    // u_long mode;
-    // ioctlsocket(sockfd, FIONBIO, &mode);  // Uncomment if you need to set the socket to unblocking mode
-    // printf("Socket mode: %lu\n", mode);
+    setup(sockfd);
+    int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
 
-    if (getsockname(sockfd, (struct sockaddr *)&local_addr, &local_addr_len) == SOCKET_ERROR) {
-        printf("getsockname failed. Error Code: %d\n", WSAGetLastError());
-        closesocket(sockfd);
-        WSACleanup();
-        return 1;
-    }
-
-    setup(sockfd, &local_addr);
-
-    fd_set read_fds, write_fds;
-    struct timeval timeout;
-    char input_buffer[BUFFER_LEN];
-
-
-    // Main Receive/Send Functionality
     while (1) {
         FD_ZERO(&read_fds);
-        FD_ZERO(&write_fds);
-
         FD_SET(sockfd, &read_fds);
-        FD_SET(sockfd, &write_fds); 
+        FD_SET(STDIN_FILENO, &read_fds);
 
         timeout.tv_sec = 5;
         timeout.tv_usec = 0;
 
-        int activity = select(sockfd + 1, &read_fds, &write_fds, NULL, &timeout);
-        if (activity == SOCKET_ERROR) {
-            printf("select() failed with error code: %d\n", WSAGetLastError());
+        int activity = select(sockfd + 1, &read_fds, NULL, NULL, &timeout);
+        if (activity < 0) {
+            perror("Select failed");
             break;
         }
 
@@ -161,23 +144,27 @@ int main() {
             receive_messages(sockfd);
         }
 
-        if (FD_ISSET(sockfd, &write_fds)) {
-            if (kbhit()) // Works perfectly for waiting for user input!
-            { 
-                fgets(input_buffer, BUFFER_LEN, stdin);
-                input_buffer[strcspn(input_buffer, "\n")] = '\0'; 
-
+        if (FD_ISSET(STDIN_FILENO, &read_fds)) {
+            if (fgets(input_buffer, BUFFER_LEN, stdin) != NULL) {
+                input_buffer[strcspn(input_buffer, "\n")] = '\0';
                 if (strlen(input_buffer) > 0) {
-                    char send_msg[BUFFER_LEN + 2];
-                    snprintf(send_msg, sizeof(send_msg), "%s\r\n", input_buffer);
-                    send_message(sockfd, send_msg);
+                    char formatted_msg[BUFFER_LEN + 2];
+                    if (strcmp(input_buffer, "/quit") == 0) {
+                        printf("Quitting...\n");
+                        break;
+                    }
+                    if (input_buffer[0] == '/') {
+                        format_command(input_buffer, formatted_msg);
+                    } else {
+                        snprintf(formatted_msg, sizeof(formatted_msg), "PRIVMSG %s :%s\r\n", CHANNEL, input_buffer);
+                    }
+                    send_message(sockfd, formatted_msg);
                 }
             }
         }
     }
 
-    closesocket(sockfd);
-    WSACleanup();
-
+    close(sockfd);
     return 0;
 }
+
